42a0d653594fd76e5e0f46ac218bbfc74f8c6f6f
MiriamShe <miriamwanjiru019@gmail.com>
2023-05-29 12:59:08 +0000

    When I was having that alphabet soup, I never thought that it would pay off

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 
-for i in range(ord('a'), ord('z'):
+for i in range(ord('a'), ord('z')+1):
     if chr(i) not in ['q', 'e']:
-	print("{}".format(chr(i)), end='')
+        print("{}".format(chr(i)), end='')
